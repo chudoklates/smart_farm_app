47834da43b7df659fc88025b3adeac1fc492624d
chudoklates <franczalko@gmail.com>
2020-03-28 18:33:44 +0100

    Wujek wladek potezny commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Being a complete fucking retard
+      </Text>
+      <Image
+        style={styles.wujekWladek}
+        source={require('./assets/itaintmuch.jpg')}
+      />
     </View>
   );
 }
@@ -16,4 +22,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  wujekWladek: {
+    height: Dimensions.get('window').height / 2,
+    width: Dimensions.get('window').width,
+    resizeMode: 'stretch'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15
+  }
 });
